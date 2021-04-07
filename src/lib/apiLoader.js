8dba4c0dfc61a56add4ec8ee718af13482cf2da5
Mike Collins <intervalia@gmail.com>
2021-04-07 08:04:33 -0600

    Updated to v0.9.10 Fixed the apiLoader to better handle a failure with loading an API file.

--- src/lib/apiLoader.js
+++ src/lib/apiLoader.js
@@ -11,6 +11,7 @@ const args = 'apimodule, require, __dirname, __filename, throw404, HttpError, Ht
 const mainPaths = require.main.paths;
 const fixPath = srcPath => srcPath.replace(/\\|[A-Z]\:[\/\\]/g, '/');
 module.paths = mainPaths;
+const ERROR_RE = /\<anonymous\>:(\d+):(\d+)/;
 
 
 class MyModule extends Module {
@@ -58,8 +59,32 @@ class MyModule extends Module {
     catch(ex) {
       // If there was a problem loading the new file then show the error
       // DO NOT pass the exception along
-      console.error(`\nFailed to load file '${fPath}'`);
-      console.error(ex.message+'\n');
+      console.error('\n---------------------------------------------')
+      console.error(`\x1B[31mFailed to load file '${fPath}'\x1B[0m`);
+      console.error(`\x1B[31m${ex.message}\x1B[0m\n`);
+
+      const temp = ex.stack.match(ERROR_RE);
+      if (temp) {
+        let line = Number(temp[1]) - 2;
+        let char = Number(temp[2]) + 1;
+        let code  = src.split('\n');
+        let output = [];
+        let i = line-3;
+        if (i < 0) {
+          i = 0;
+        }
+        for(; i < line + 4; i++) {
+          if (i === (line - 1)) {
+            output.push(`  \x1B[41m${code[i]}\x1B[0m`);
+            output.push(`\x1B[93m${'─'.repeat(char)}┘\x1B[0m`);
+          }
+          else {
+            output.push(`  \x1B[92m${code[i]}\x1B[0m`);
+          }
+        }
+        console.error(output.join('\n')+'\n');
+      }
+      this.exports = {};
     }
 
     return this.exports;
